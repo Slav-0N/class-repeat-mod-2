13b7d85e23c51757c0df3736df94dab0b96a03c9
Slav-0N <SOVILGO@gmail.com>
2024-02-02 19:48:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import FirstClassComponent from "./Components/FirstClassComponent.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello!</div>
+      <FirstClassComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/FirstClassComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FirstClassComponent extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return <div>ClassComponent</div>;
+  }
+}
+
+export default FirstClassComponent;
